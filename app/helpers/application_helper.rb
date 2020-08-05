ec03fc2aa517fdbc3f84098cc1b97bcab4314cbc
Abhaya <thapa.abhay@gmail.com>
2020-08-05 23:51:20 +0545

    Font awesome added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def sort_link_turbo(attribute, *args)
+    sort_link(attribute, *args.push({}, { data: { turbolinks_action: 'replace' } }))
+  end
+
+  def icon(klass, text = nil)
+    icon_tag = tag.i(class: klass)
+    text_tag = tag.span text
+    text ? tag.span(icon_tag + text_tag) : icon_tag
+  end
 end
