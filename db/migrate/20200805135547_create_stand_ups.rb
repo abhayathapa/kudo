c2ef7d604d0d54dc70a586f17a26696a6e3a3e70
Abhaya <thapa.abhay@gmail.com>
2020-08-05 21:08:43 +0545

    Devise added

--- db/migrate/20200805135547_create_stand_ups.rb
+++ db/migrate/20200805135547_create_stand_ups.rb
@@ -1,7 +1,11 @@
 class CreateStandUps < ActiveRecord::Migration[6.0]
   def change
     create_table :stand_ups do |t|
+      t.string :name ,null: false
+      t.string :hours ,null: false
+      t.text :info
 
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
